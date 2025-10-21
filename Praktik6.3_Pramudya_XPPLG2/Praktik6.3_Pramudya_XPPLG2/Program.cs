07d9035048d566f74271a193c69c335c9997413b
pramudya258769-stack <pramudya258769@student.smkn1kandeman.sch.id>
2025-10-20 20:00:07 -0700

    Add files via upload

---
+++ Praktik6.3_Pramudya_XPPLG2/Praktik6.3_Pramudya_XPPLG2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praktik6._3_Pramudya_XPPLG2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Inisialisasi variabel untuk menyimpan input
+            string inputUser = "";
+
+            Console.WriteLine("Program akan terus berjalan hingga anda mengetik 'keluar'.");
+
+            // Perulangan selama ini variabel inputUser TIDAK SAMA DENGAN "keluar"
+            while (inputUser.ToLower() != "keluar")
+            {
+                Console.WriteLine("\nKetik sesuatu (atau 'keluar' untuk berhenti): ");
+                inputUser = Console.ReadLine(); // Baca input dari user
+
+                Console.WriteLine("Anda mengetik: " + inputUser);
+            }
+                Console.WriteLine("\nProgram selesai. Terima kasih !");
+        }
+    }
+}
