15b74f9578bf14a7c7c90574b110545b3ca18938
pramudya258769-stack <pramudya258769@student.smkn1kandeman.sch.id>
2025-10-20 19:32:13 -0700

    Add files via upload

---
+++ Tugas6.9_Pramudya_XPPLG2/Tugas6.9_Pramudya_XPPLG2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tugas6._9_Pramudya_XPPLG2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int total = 0;
+            Console.WriteLine("Menghitung Perkalian 5: ");
+
+            for (int i = 1; i <= 10; i++)
+            {
+                total = total * i;
+                Console.WriteLine($"5 x {i} = {5 * i}");
+
+            }
+
+        }
+    }
+}
