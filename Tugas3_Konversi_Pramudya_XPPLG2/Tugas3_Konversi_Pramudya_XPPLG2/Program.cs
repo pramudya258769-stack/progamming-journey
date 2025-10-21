bc72282738b683d77160cbefd93c262ce1cbc63a
pramudya258769-stack <pramudya258769@student.smkn1kandeman.sch.id>
2025-10-21 01:13:06 -0700

    Add files via upload

---
+++ Tugas3_Konversi_Pramudya_XPPLG2/Tugas3_Konversi_Pramudya_XPPLG2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tugas3_Konversi_Pramudya_XPPLG2
+{
+    internal class Program
+    {
+        static double fahr (double suhu1)
+        {
+            return (suhu1 * 9 / 5) + 32;
+        } 
+        static void Main(string[] args)
+        {
+                    Console.Write("Masukkan suhu dalam bentuk celcius: ");
+                    double c = Convert.ToDouble(Console.ReadLine());
+
+                    double fahrenheit = fahr(c);
+                    Console.WriteLine("Hasil konversi celcius ke Fahrenheit: " + fahrenheit + "°C");
+        }
+    }
+}
