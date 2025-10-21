89d5cbe0cc6e15dd90f242f33813795391b9884d
pramudya258769-stack <pramudya258769@student.smkn1kandeman.sch.id>
2025-10-20 23:24:38 -0700

    Add files via upload

---
+++ Praktik7.1_Pramudya_XPPLG2/Praktik7.1_Pramudya_XPPLG2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praktik7._1_Pramudya_XPPLG2
+{
+    internal class Program
+    {
+        static void Sapa()
+        {
+            Console.WriteLine("Halo, selamat datang di pembelajaran!");
+            Console.WriteLine("Mari belajar tentang fungsi!");
+        }
+        static void Main(string[] args)
+        {
+            Sapa(); // Memanggil fungsi
+        }
+    }
+}
