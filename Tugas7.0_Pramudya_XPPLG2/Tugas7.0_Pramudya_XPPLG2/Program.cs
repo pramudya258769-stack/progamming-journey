8eae15cf7671da7956ca8a6b5b56425148884474
pramudya258769-stack <pramudya258769@student.smkn1kandeman.sch.id>
2025-10-20 19:26:02 -0700

    Add files via upload

---
+++ Tugas7.0_Pramudya_XPPLG2/Tugas7.0_Pramudya_XPPLG2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tugas7._0_Pramudya_XPPLG2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Menampilkan jumlah total dari bilangan 1-5: ");
+
+            int total = 0;
+            int penambah = 1;
+
+            Console.WriteLine("Nilai awal: " + total);
+
+            while (total < 15)
+            {
+                Console.WriteLine("Nilai ditambah dengan " + penambah);
+                total = total + penambah;
+                penambah++;
+                Console.WriteLine("Nilai saat ini = " + total + "\n");
+            }
+        }
+    }
+}
