6da624d388a0ae7bebe2926ec5ca946770814a93
pramudya258769-stack <pramudya258769@student.smkn1kandeman.sch.id>
2025-10-20 23:26:48 -0700

    Add files via upload

---
+++ Praktik7.3_Luas_Pramudya_XPPLG2/Praktik7.3_Luas_Pramudya_XPPLG2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praktik7._3_Luas_Pramudya_XPPLG2
+{
+    internal class Program
+    {
+        static double HitungLuas(double panjang, double lebar)
+        {
+            double luas = panjang * lebar;
+            return luas;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Masukkan panjang: ");
+            double p = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Masukkan lebar: ");
+            double l = Convert.ToDouble(Console.ReadLine());
+
+            double hasil = HitungLuas(p, l);
+            Console.WriteLine("Luas persegi panjang adalah: " + hasil);
+        }
+    }
+}
