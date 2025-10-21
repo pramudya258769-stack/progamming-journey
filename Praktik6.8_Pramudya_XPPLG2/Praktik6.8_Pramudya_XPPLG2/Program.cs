6c4eb0cbc100b21d1ae748e1160091f9f64fb2d5
pramudya258769-stack <pramudya258769@student.smkn1kandeman.sch.id>
2025-10-20 19:33:45 -0700

    Add files via upload

---
+++ Praktik6.8_Pramudya_XPPLG2/Praktik6.8_Pramudya_XPPLG2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praktik6._8_Pramudya_XPPLG2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string ulangiPilihan;
+
+            do
+            {
+                // --- Blok Proses yang Akan Diulang ---
+                Console.WriteLine("------------------------------------");
+                Console.Write("Masukkan Nama Anda: ");
+                string nama = Console.ReadLine();
+                Console.WriteLine($"Halo, {nama}! Proses telah selesai.");
+                //---------------------------------------
+                // Pertanyaan untuk Mengulang
+                Console.Write("Apakah Anda Ingin Mengulang Lagi? (ya/tidak): ");
+                ulangiPilihan = Console.ReadLine().ToLower(); // Mengambil input dan mengubah ke huruf kecil
+
+                Console.WriteLine(); // Baris kosong untuk kerapian 
+
+                // Kondisi diperiksa di akhir: Ulangi selama pengguna mengetik "ya"
+            } while (ulangiPilihan == "ya");
+            Console.WriteLine("Terima kasih. Program selesai.");
+        }
+    }
+}
