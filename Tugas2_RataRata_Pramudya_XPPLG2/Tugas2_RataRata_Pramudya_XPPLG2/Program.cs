ee2f3e31e6acff731ba60b08794d7aa41ea441af
pramudya258769-stack <pramudya258769@student.smkn1kandeman.sch.id>
2025-10-21 00:44:24 -0700

    Add files via upload

---
+++ Tugas2_RataRata_Pramudya_XPPLG2/Tugas2_RataRata_Pramudya_XPPLG2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tugas2_RataRata_Pramudya_XPPLG2
+{
+    internal class Program
+    {
+        static double RataRata(double angka1, double angka2, double angka3)
+        {
+            return (angka1 + angka2 + angka3) / 3;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Masukkan nilai pertama \t: ");
+            double angka1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Masukkan nilai kedua \t: ");
+            double angka2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Masukkan nilai ketiga \t: ");
+            double angka3 = Convert.ToDouble(Console.ReadLine());
+
+            double rataRata = (angka1 +  angka2 + angka3) /3;
+            Console.WriteLine("Hasil Rata-Rata nilai \t: " + rataRata);
+        }
+    }
+}
